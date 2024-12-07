56644ac1c99c8a906612f8f53c8fb7aa5f8d93d8
Olatunji Oluwadare <tunjidare2@yahoo.com>
2024-12-07 16:43:35 +0100

    File to initiate flask application

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,1 +1,11 @@
-i
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return {"message": "Welcome to Freelance 9ja!"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
