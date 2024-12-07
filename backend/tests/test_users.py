b99a735f480a23a036cca23751efb1bb60266b5b
Olatunji Oluwadare <tunjidare2@yahoo.com>
2024-12-07 17:05:40 +0100

    Postman to test the /register endpoint

---
+++ backend/tests/test_users.py
@@ -0,0 +1,11 @@
+import pytest
+from app import create_app, db
+from app.models import User
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['TESTING'] = True
+    return app
+
